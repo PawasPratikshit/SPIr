ee0d192b0ac57fd58c1627a53cd91c0af62dbafe
Pawas Pratikshit <pawas2480@gmail.com>
2025-11-28 01:42:14 +0530

    Polish startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,5 @@
 
 #' @keywords internal
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    paste0(
-      "SPIr loaded.\n",
-      "  • Compute SPI for your data:  compute_spi(data, pc_id, ac_id, candidate, votes, ...)\n",
-      "  • Add SPI back to full data:  add_spi(data, pc_id, ac_id, candidate, votes, ...)\n",
-      "  • Quick summary:              summary_spi(spi_data, spi)\n",
-      "  • Plot distribution:          plot_spi_distribution(spi_data, spi)\n",
-      "Type ?compute_spi for full documentation."
-    )
-  )
+  message("### SPIr ATTACHED ###")
 }
